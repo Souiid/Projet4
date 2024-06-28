941a9f940ef59ac72b504be27c68b8f5d6b2439c
idrisssouissi <idriss.souissi12@gmail.com>
2024-06-28 12:43:51 +0200

    Update javadoc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.kapt")
     id("dagger.hilt.android.plugin")
+    id("org.jetbrains.dokka") version "1.7.20"
+
 }
 
 android {
